11ee1d6d796d97c5b05dcc5691352f00f25b97c7
Pupix <manolea.robert@gmail.com>
2017-12-11 01:20:39 +0100

    Switched out ps-node for lcu-connector

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
+const electron = require('electron');
+const LCUConnector = require('lcu-connector');
 
-    const electron = require('electron');
-    const {app} = electron;
-    const {BrowserWindow} = electron;
-    const root = __dirname + '/app';
+const connector = new LCUConnector();
+const { app } = electron;
+const { BrowserWindow } = electron;
+const root = __dirname + '/app';
 
-    const UX = require('./lib/ux');
+let mainWindow = null;
 
-    let mainWindow = null;
+app.commandLine.appendSwitch('--ignore-certificate-errors');
 
-    app.commandLine.appendSwitch('--ignore-certificate-errors');
+app.on('ready', () => {
 
-    app.on('ready', () => {
-
-        mainWindow = new BrowserWindow({
-            center: true,
-            height: 720,
-            show: false,
-            title: 'League API',
-            width: 1280
-        });
-
-        // Remove default menu
-        mainWindow.setMenu(null);
-
-        mainWindow.loadURL('file://' + root + '/index.html');
-
-        // Check if dev env FIXME
-        //mainWindow.openDevTools();
-
-        // Avoid white page on load.
-        mainWindow.webContents.on('did-finish-load', () => {
-            UX.getArguments()
-                .then((result) => mainWindow.webContents.send('arguments-load', result))
-                .catch((error) => mainWindow.webContents.send('arguments-error', error.message));
-
-            mainWindow.show();
-        });
-
-        mainWindow.on('closed', () => {
-            mainWindow = null;
-        });
+    mainWindow = new BrowserWindow({
+        center: true,
+        height: 720,
+        show: false,
+        title: 'Rift Explorer',
+        width: 1280
     });
 
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
+    // Remove default menu
+    mainWindow.setMenu(null);
+    mainWindow.loadURL('file://' + root + '/index.html');
+
+    // Check if dev env FIXME
+    //mainWindow.openDevTools();
+
+    // Avoid white page on load.
+    mainWindow.webContents.on('did-finish-load', () => {
+        connector.on('connect', (data) => {
+            mainWindow.webContents.send('lcu-load', data);
+        });
+
+        connector.start();
+        mainWindow.show();
     });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
